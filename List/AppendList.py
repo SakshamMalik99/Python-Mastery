b5017158c50505ecd2357cbc623d8ddb89d1deeb
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-16 12:29:13 +0530

    Add files via upload

---
+++ List/AppendList.py
@@ -0,0 +1,13 @@
+animal = ['cat', 'dog', 'rabbit']
+
+animal.append('guinea pig ')
+
+print('Updated animal list: ', animal)
+
+animal = ['cat', 'dog', 'rabbit']
+
+wild_animal = ['tiger', 'fox']
+
+animal.append(wild_animal)
+
+print('Updated animal list: ', animal)
