2e98c26d27af104626e61a53ef6a359113319761
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-16 12:32:32 +0530

    Add files via upload

---
+++ List/ExtendedMethod.py
@@ -0,0 +1,26 @@
+language = ['French', 'English', 'German']
+
+# another list of language
+language1 = ['Spanish', 'Portuguese']
+
+language.extend(language1)
+
+
+# language list
+language = ['French', 'English', 'German']
+
+# language tuple
+language_tuple = ('Spanish', 'Portuguese')
+
+# language set
+language_set = {'Chinese', 'Japanese'}
+
+# appending element of language tuple
+language.extend(language_tuple)
+
+print('New Language List: ', language)
+
+# appending element of language set
+language.extend(language_set)
+
+print('Newest Language List: ', language)
