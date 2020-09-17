77feb3753477232fec80a4f661c7f6570b896869
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-17 10:54:03 +0530

    Add files via upload

---
+++ List/InsertMethod.py
@@ -0,0 +1,13 @@
+vowel = ['a', 'e', 'i', 'u']
+
+vowel.insert(3, 'o')
+
+print('Updated List: ', vowel)
+
+mixed_list = [{1, 2}, [5, 6, 7]]
+
+number_tuple = (3, 4)
+
+mixed_list.insert(1, number_tuple)
+
+print('Updated List: ', mixed_list)
