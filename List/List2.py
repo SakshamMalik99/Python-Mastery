20d300c4cf216b00f61310eac2b5ae5a450013fc
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-17 11:44:34 +0530

    Add files via upload

---
+++ List/List2.py
@@ -0,0 +1,12 @@
+print("Program :2 :")
+# slice lists
+my_list = ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+print(my_list)
+# elements 3rd to 5th
+print(my_list[2:5])
+# elements beginning to 4th
+print(my_list[:-5])
+# elements 6th to end
+print(my_list[5:])
+# elements beginning to end
+print(my_list[:])
